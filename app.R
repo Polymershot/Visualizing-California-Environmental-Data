2c0698dbe7f364ad61eb8ded69aae6c6375b102a
Randy Neou <rdn9177@gmail.com>
2024-05-17 14:35:35 -0700

    Initial commit

---
+++ app.R
@@ -0,0 +1,299 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shinydashboard) #Used for boxes(), fluidPage(), fluidRow()
+library(DT) #Used for Datatable rendering
+library(shinyWidgets) #pickerINput; select all button
+library(name) #shiny icons?
+library(leaflet) #interactive maps
+library(rcompanion) #histogram with normal curve
+library(mapview) #to create maps from shp files
+library(RColorBrewer) #create color schemes for maps
+library(leafpop) #modify popup table for mapview maps
+library(heplots) #chi-square plots
+library(ggpubr) #qqplot
+
+
+source("scripts/preprocessing.R")
+
+# Define UI for application 
+ui <- dashboardPage(
+  
+    # Application title
+    dashboardHeader(
+      title = "CalEnviro 4.0 Data"
+    ),
+    
+    dashboardSidebar(
+      sidebarMenu(
+        id = 'sidebar',
+        menuItem('Information', tabName = 'info', icon = icon('circle-info')),
+        menuItem('Dataset', tabName = 'dataset', icon = icon('table')),
+        menuItem('Filter Map By County', tabName = 'county', icon = icon('map')),
+        menuItem('Filter Map By City', tabName = 'city', icon = icon('map')),
+        menuItem('Normality', tabName = 'normality', icon = icon('chart-simple'))
+      ),
+      
+      conditionalPanel(
+        pickerInput(
+          inputId = 'column',
+          'Choose some columns',
+          choices = colnames(master_df),
+          selected = colnames(master_df)[1:10],
+          options = list(`actions-box` = TRUE, `live-search`=TRUE),
+          multiple = T
+        ), condition = " input.sidebar == 'dataset' "
+      ),
+      
+      conditionalPanel(
+        pickerInput(
+        inputId = 'county',
+        'Choose A County',
+        choices = county_list,
+        options = list(`actions-box` = TRUE, `live-search`=TRUE),
+        multiple = F
+      ),
+        condition = " input.sidebar == 'county' "
+    ),
+    
+      conditionalPanel(
+        pickerInput(
+          inputId = 'popup_col_county',
+          'Choose variable of interest (click on map areas for variable information)',
+          choices = shp_master_names[!(shp_master_names %in% unwanted_vars)],
+          selected = c(
+            "total_population"
+          ),
+          options = list(`live-search`=TRUE, `dropup-auto`=FALSE),
+          multiple = F
+        ), condition = " input.sidebar == 'county' "
+      ),
+    
+    conditionalPanel(
+      pickerInput(
+        inputId = 'color_col_county',
+        'Choose variable to show color for',
+        choices = c(scientific_cols, ethnicity_cols, 'ces_4_0_score'),
+        options = list(`actions-box` = TRUE, `live-search`=TRUE, `dropup-auto`=FALSE),
+        multiple = F
+      ), condition = " input.sidebar == 'county' "
+    ),
+    
+    conditionalPanel(
+      pickerInput(
+        inputId = 'city',
+        'Choose A City',
+        choices = unique(as.list(shp_and_master$approximate_location)),
+        options = list(`actions-box` = TRUE, `live-search`=TRUE),
+        multiple = F
+      ),
+      condition = " input.sidebar == 'city' "
+    ),
+    
+    conditionalPanel(
+      pickerInput(
+        inputId = 'popup_col_city',
+        'Choose variables of interest (click on map areas for variable information)',
+        choices = shp_master_names[!(shp_master_names %in% unwanted_vars)],
+        selected = c(
+          "total_population"
+        ),
+        options = list(`live-search`=TRUE, `actions-box` = TRUE,  `dropup-auto`=FALSE),
+        multiple = T
+      ), condition = " input.sidebar == 'city' "
+    ),
+    
+    conditionalPanel(
+      pickerInput(
+        inputId = 'color_col_city',
+        'Choose variable to show color for',
+        choices = c(scientific_cols, ethnicity_cols, 'ces_4_0_score'),
+        options = list(`actions-box` = TRUE, `live-search`=TRUE, `dropup-auto`=FALSE),
+        multiple = F
+      ), condition = " input.sidebar == 'city' "
+    ),
+    
+    
+    
+    conditionalPanel(
+      pickerInput(
+        inputId = 'normality_col',
+        'Choose variable of interest',
+        choices = c(scientific_cols, ethnicity_cols),
+        options = list(`actions-box` = TRUE, `live-search`=TRUE),
+        multiple = F
+      ), condition = " input.sidebar == 'normality' "
+    )
+    ),
+    dashboardBody(
+      tabItems(
+        tabItem(
+          tabName = 'dataset',
+          DTOutput('master_df')
+        ),
+        
+        tabItem(
+          tabName = 'county',
+          leafletOutput("countymap", height = 780)
+        ),
+        
+        tabItem(
+          tabName = 'city',
+          leafletOutput("citymap", height=780)
+        ),
+        
+        tabItem(
+          tabName = 'normality',
+          fluidPage(
+            fluidRow(
+              box(plotOutput('histogram')),
+              box(plotOutput('qqplot'))
+            ),
+            fluidRow(
+              column(box(plotOutput('cqplot')), width=12, offset = 3)
+            ),
+            fluidRow(
+                div(
+                p("The Chi-Square plot shows us that the CalEnviro4.0 data is not multivariate normal for the selected columns; which is to be expected."),
+                align="center"
+                )
+            )
+            
+          )
+        ),
+        
+        tabItem(
+          tabName = 'info',
+          fluidPage(
+            uiOutput("pdf")
+          )
+        )
+       
+    
+      )
+    )
+        
+    )
+  
+
+
+
+# Define server logic
+server <- function(input, output) {
+  
+  #Information
+  addResourcePath("folder",paste0(getwd()))
+  output$pdf <- renderUI({
+    tags$iframe(style="height:785px; width:100%", src="folder/calenviroreport.pdf")
+  })
+  
+
+  #Dataset 
+  choose_column <- eventReactive(input$column, {
+    master_df %>% select(input$column)
+  })
+  
+  
+  #Filter by County
+  choose_shpmaster <- eventReactive(input$county, {
+    shp_and_master %>% filter(california_county %in% input$county)
+  })
+  
+  choose_countyshp <- eventReactive(input$county, {
+    county_shp %>% filter(NAMELSAD %in% input$county)
+  })
+
+  choose_popup_cols_county <- eventReactive(input$popup_col_county, {
+    shp_and_master %>% select(input$popup_col_county)
+  })
+  
+  choose_color_col_county <- eventReactive(input$color_col_county, {
+    shp_and_master %>% select(input$color_col_county)
+  })
+  
+  
+  
+  #Filter by City
+  choose_shpmaster2 <- eventReactive(input$city, {
+    shp_and_master %>% filter(approximate_location == input$city)
+  })
+  
+  choose_popup_cols_city <- eventReactive(input$popup_col_city, {
+    shp_and_master %>% select(input$popup_col_city)
+  })
+  
+  choose_color_col_city <- eventReactive(input$color_col_city, {
+    shp_and_master %>% select(input$color_col_city)
+  })
+  
+  #Normality
+  choose_normal_col <- eventReactive(input$normality_col, {
+    master_df %>% select(input$normality_col)
+  })
+
+  
+  #Dataset
+  output$master_df <- renderDT(choose_column())
+  
+  #Filter by County
+  output$countymap <- renderLeaflet(
+    {
+    (mapview(choose_countyshp(), zcol = "NAMELSAD", burst = FALSE, alpha.regions= 0.1, lwd = 3, popup = FALSE) +
+      mapview(choose_shpmaster(),
+               zcol =input$color_col_county, 
+               col.regions = brewer.pal(7, "Dark2"), na.color = "#A9A9A9",
+               popup = popupTable(choose_shpmaster(),
+                                  zcol = colnames(choose_popup_cols_county())[1:(ncol(choose_popup_cols_county())-1)]
+               )
+       )
+  )@map
+  })
+  
+  
+  #Filter by City
+  output$citymap <- renderLeaflet(
+    {
+    (mapview(choose_shpmaster2(),
+                 zcol =input$color_col_city, 
+                 col.regions = brewer.pal(7, "Dark2"), na.color = "#A9A9A9",
+                 popup = popupTable(choose_shpmaster2(),
+                                    zcol = colnames(choose_popup_cols_city())[1:(ncol(choose_popup_cols_city())-1)]
+                                      
+                 )
+            )
+    )@map
+      
+    })
+  
+  #Normality
+  output$histogram <- renderPlot({
+    plotNormalHistogram(master_df[[input$normality_col]], breaks = "FD", main = paste("Histogram of", input$normality_col),
+         xlab = input$normality_col, ylab = "Frequency")
+    })
+
+  
+  output$qqplot <- renderPlot(
+    {
+      qqnorm(master_df[[input$normality_col]], main = paste("QQ Plot of", input$normality_col))
+      qqline(master_df[[input$normality_col]])
+    }
+  )
+  
+  output$cqplot <- renderPlot(
+   {cqplot(x=data, conf=0.95)}
+  )
+
+ 
+
+  
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
---
+++ scripts/preprocessing.R
@@ -0,0 +1,93 @@
+library(tidyverse) #the best!
+library(sf) #load in shp file and change into df
+library(readxl) #read excel files
+library(janitor) #to clean names
+library(renv)
+
+
+#Run code
+read_excel_allsheets <- function(filename, tibble = TRUE) {
+  # I prefer straight data.frames
+  # but if you like tidyverse tibbles (the default with read_excel)
+  # then just pass tibble = TRUE
+  sheets <- readxl::excel_sheets(filename)
+  x <- lapply(sheets, function(X) readxl::read_excel(filename, sheet = X))
+  if(!tibble) x <- lapply(x, as.data.frame)
+  names(x) <- sheets
+  x
+}
+
+mysheets <- read_excel_allsheets("data/California-Environment-Data/envirodata.xlsx")
+mysheets[[2]] <- mysheets[[2]] %>% row_to_names(row_number = 1)
+names(mysheets) <- c("raw_data", "demographic_data")
+
+#Change column types for science_data
+mysheets[[1]]$`Census Tract` <- as.character(mysheets[[1]]$`Census Tract`)
+varnames = c("Lead", "Lead Pctl", "Low Birth Weight", "Low Birth Weight Pctl","Education", "Education Pctl", "Linguistic Isolation", "Linguistic Isolation Pctl", "Unemployment", "Unemployment Pctl", "Housing Burden", "Housing Burden Pctl")
+mysheets[[1]] <- mysheets[[1]] %>% mutate_if(names(.) %in% varnames, funs(as.numeric(.)))
+
+
+#Change column types for demographic_data
+varnames = names(mysheets[[2]][, -which(names(mysheets[[2]]) %in% c("Census Tract", "CES 4.0 Percentile Range", "California County"))])
+mysheets[[2]] <- mysheets[[2]] %>% mutate_if(names(.) %in% varnames, funs(as.numeric(.)))
+
+#Combine the datasets
+master_df <- inner_join(mysheets[[1]], mysheets[[2]]) %>% clean_names() %>%
+  rename(
+    ces_4.0_score = ces_4_0_score,
+    ces_4.0_percentile = ces_4_0_percentile,
+    ces_4.0_percentile_range = ces_4_0_percentile_range,
+    pm_2.5 = pm2_5,
+    pm_2.5_pctl = pm2_5_pctl,
+    children_less_than_10_years_percent = children_10_years_percent,
+    pop_10_through_64_years_percent = pop_10_64_years_percent,
+    elderly_greater_than_64_years_percent = elderly_64_years_percent
+  )
+
+#Read in shp
+census_shp <- st_read("data/Census-Tracts-(Tigerline)/tl_2023_06_tract.shp")
+county_shp <- st_read("data/County-Boundaries/CA_Counties_TIGER2016.shp")
+census_tibble <- tibble(census_shp)
+
+#Remove 0's from census tracts
+census_tibble$GEOID <- gsub("^0", "", census_tibble$GEOID)
+
+#Create copy of census_shp
+census_shp_remove0s <- census_shp
+census_shp_remove0s$GEOID <- gsub("^0", "", census_shp_remove0s$GEOID)
+
+#Join master and census shp
+shp_and_master <- right_join(census_shp_remove0s, master_df, by = join_by("GEOID" == "census_tract")) %>% clean_names()
+shp_and_master$california_county <- paste0(shp_and_master$california_county, " County")
+
+#Create list of counties
+county_list <-  unique(county_shp$NAMELSAD)
+
+
+#Select all columns associated with ethnicity/population characteristics
+ethnicity_cols <- names(mysheets[[2]] %>% select(!c("Census Tract", "CES 4.0 Score", "CES 4.0 Percentile", "CES 4.0 Percentile Range", "California County")) %>% clean_names() %>% rename(
+  children_less_than_10_years_percent = children_10_years_percent,
+  pop_10_through_64_years_percent = pop_10_64_years_percent,
+  elderly_greater_than_64_years_percent = elderly_64_years_percent
+));ethnicity_cols
+
+#Select all columns associated with scientific measures and percentile variables
+scientific_cols <- names(mysheets[[1]] %>% select(!c("Census Tract", "CES 4.0 Percentile Range", "Total Population", "California County" ,"ZIP", "Approximate Location", "Longitude", "Latitude", "CES 4.0 Score", "CES 4.0 Percentile", "Pollution Burden", "Pop. Char." ), -matches("Pctl")) %>% clean_names() %>% rename(
+  pm_2.5 = pm2_5
+));scientific_cols
+
+#Percentile Columns
+scientific_cols_pctl <- names(mysheets[[1]] %>% select(!c("Census Tract", "CES 4.0 Percentile Range", "California County" ,"ZIP", "Approximate Location", "Longitude", "Latitude", "CES 4.0 Score", "CES 4.0 Percentile", "Pollution Burden", "Pop. Char." ), matches("Pctl")) %>% clean_names() %>% rename(
+  pm_2.5 = pm2_5
+) %>% select(matches("Pctl")));scientific_cols_pctl
+
+
+#Complete Case Data
+complete_case <- master_df[complete.cases(master_df),]
+data <- complete_case %>% dplyr::select(ethnicity_cols, scientific_cols)
+
+
+#list of variables to not choose
+unwanted_vars = c("statefp", "countyfp", "tractce", "geoid", "geoidfq", "name", "namelsad", "mtfcc", "longitude", "latitude", "funcstat", "aland", "awater", "intptlat", "intptlon","geometry")
+shp_master_names = colnames(shp_and_master)
+
